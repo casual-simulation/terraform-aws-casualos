440cc7477d20e85590a060a09c7eba70a26b20a6
Kallyn Gowdy <kallyngowdy@gmail.com>
2020-05-04 17:00:21 -0400

    fix: Use the Ubuntu distribution of Docker instead of the Docker one
    - Doesn't need the docker apt repo to be installed.
    - Works on new versions of Ubuntu automatically.

--- modules/nomad-consul-ami/install-docker.sh
+++ modules/nomad-consul-ami/install-docker.sh
@@ -14,19 +14,8 @@ sudo apt-get install -y \
     gnupg-agent \
     software-properties-common
 
-# Download and install the Docker repository GPG key
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-
-# Add the Docker repository
-# TODO: Support ARM64
-sudo add-apt-repository \
-   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
-   $(lsb_release -cs) \
-   stable"
-
 # Install Docker
-sudo apt-get update || sudo apt update -y
-sudo apt-get install -y docker-ce docker-ce-cli containerd.io
+sudo apt install -y docker.io
 
 # Add the ubuntu user to the docker group
 sudo usermod -a -G docker ubuntu
