1189c688285f2e28de29cdddb2f1cc23df1adc17
Kallyn Gowdy <kallyngowdy@gmail.com>
2020-05-04 17:37:49 -0400

    fix: Ensure the scripts are executable before running them

--- modules/nomad-consul-ami/install-consul.sh
+++ modules/nomad-consul-ami/install-consul.sh
@@ -1,1 +1,2 @@
+sudo chmod +x /tmp/install-consul.sh
 /tmp/install-consul.sh --version "${CONSUL_VERSION}"
--- modules/nomad-consul-ami/install-nomad.sh
+++ modules/nomad-consul-ami/install-nomad.sh
@@ -1,1 +1,2 @@
+sudo chmod +x /tmp/install-nomad.sh
 /tmp/install-nomad.sh --version "${NOMAD_VERSION}"
