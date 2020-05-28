d7d2b9e112a899d064ebbbb1ce96f0285b9da763
Kallyn Gowdy <kallyngowdy@gmail.com>
2020-05-28 19:23:59 -0400

    feat: Use names that won't conflict

--- modules/casualos-micro/main.tf
+++ modules/casualos-micro/main.tf
@@ -375,7 +375,7 @@ resource "aws_ebs_volume" "mongodb" {
 }
 
 resource "aws_iam_role" "dlm_lifecycle_role" {
-  name = "dlm-lifecycle-role"
+  name = "${var.name}-dlm-lifecycle-role"
 
   assume_role_policy = <<EOF
 {
@@ -395,7 +395,7 @@ EOF
 }
 
 resource "aws_iam_role_policy" "dlm_lifecycle" {
-  name = "dlm-lifecycle-policy"
+  name = "${var.name}-dlm-lifecycle-policy"
   role = aws_iam_role.dlm_lifecycle_role.id
 
   policy = <<EOF
